42f88dc21dcef4b4d1496e3e7194a1eb09bd2040
HeejaeSeo <chungyounglove@gmail.com>
2020-03-18 14:47:18 +0900

    01_DNA

---
+++ 01_Counting_DNA_Nucleotides.py
@@ -0,0 +1,23 @@
+### 01. Counting DNA Nucleotides
+## A rapid Introduction to Molecular Biology
+
+# Date : 19.06.12 (Wed)
+
+## 01. Read File
+
+with open("rosalind_dna.txt") as f :
+	fr = f.readline()
+	fr = fr.replace("\n", "")
+
+
+## 02. Count the Number of Bases
+
+cnt_a = fr.count("A")
+cnt_c = fr.count("C")
+cnt_g = fr.count("G")
+cnt_t = fr.count("T")
+
+
+## 03. Print the Number of Bases
+
+print("%d %d %d %d" %(cnt_a, cnt_c, cnt_g, cnt_t))
